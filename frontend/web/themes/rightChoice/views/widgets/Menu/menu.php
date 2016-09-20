e09f49e97f192c7824b1391fb04567287e78ec9f
mifort1 <mifort@ukr.net>
2016-09-20 16:09:11 +0300

    menu ru

--- frontend/web/themes/rightChoice/views/widgets/Menu/menu.php
+++ frontend/web/themes/rightChoice/views/widgets/Menu/menu.php
@@ -3,141 +3,23 @@
     <!--LISTS-->
     <ul class="menu clearfix">
         <li>
-            <a href="index.html" title="Home">Home</a>
-            <div class="dropdown span3">
-                <div class="span2">
-                    <ul>
-                        <li><a href="index.html">Home (CarouFredSel)</a></li>
-                        <li><a href="index2.html">Home (Elastic Slider)</a></li>
-                        <li><a href="index-boxed.html">Home (boxed version)</a></li>
-                    </ul>
-                </div>
-            </div>
+            <a href="index.html" title="Каталог товаров">Каталог товаров</a>
+
         </li>
         <li>
-            <a>Men</a>
-            <div class="dropdown span9">
-                <div class="span2">
-                    <h4>Shirts</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Oxford Shirts</a></li>
-                        <li><a href="products-grid.html">Check Shirts</a></li>
-                        <li><a href="products-grid.html">Denim Shirts</a></li>
-                        <li><a href="products-grid.html">Striped Shirts</a></li>
-                        <li><a href="products-grid.html">Printed Shirts</a></li>
-                    </ul>
-                </div>
-                <div class="span2">
-                    <h4>Shoes</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Boots</a></li>
-                        <li><a href="products-grid.html">Desert Boots</a></li>
-                        <li><a href="products-grid.html">Boat Shoes</a></li>
-                        <li><a href="products-grid.html">Format Shoes</a></li>
-                        <li><a href="products-grid.html">Trainers</a></li>
-                    </ul>
-                </div>
-                <div class="span2">
-                    <h4>Jackets</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Blazers</a></li>
-                        <li><a href="products-grid.html">Leather Jackets</a></li>
-                        <li><a href="products-grid.html">Denim Jackets</a></li>
-                        <li><a href="products-grid.html">Gilets</a></li>
-                    </ul>
-                </div>
-                <div class="menu-portrait fr">
-                    <img src="<?php echo $this->theme->baseUrl;?>/assets/img/guy.jpg"/>
-                </div>
-            </div>
+            <a href="index.html" title="Новинки">Новинки</a>
+
         </li>
         <li>
-            <a>Women</a>
-            <div class="dropdown span9">
-                <div class="span2">
-                    <h4>Dresses</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Evening Dresses</a></li>
-                        <li><a href="products-grid.html">Casual Dresses</a></li>
-                        <li><a href="products-grid.html">Party Dresses</a></li>
-                        <li><a href="products-grid.html">Maxi Dresses</a></li>
-                        <li><a href="products-grid.html">Midi Dresses</a></li>
-                        <li><a href="products-grid.html">Strapless Dresses</a></li>
-                    </ul>
-                </div>
-                <div class="span2">
-                    <h4>Accessories</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Bags & Purses</a></li>
-                        <li><a href="products-grid.html">Belts</a></li>
-                        <li><a href="products-grid.html">Collars</a></li>
-                        <li><a href="products-grid.html">Gloves</a></li>
-                        <li><a href="products-grid.html">Hats & Caps</a></li>
-                        <li><a href="products-grid.html">Sunglasses</a></li>
-                    </ul>
-                </div>
-                <div class="span2">
-                    <h4>Tops</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Blouses</a></li>
-                        <li><a href="products-grid.html">Bodies</a></li>
-                        <li><a href="products-grid.html">Evening Tops</a></li>
-                        <li><a href="products-grid.html">Shirts</a></li>
-                    </ul>
-                </div>
-                <div class="menu-portrait fr">
-                    <img src="<?php echo $this->theme->baseUrl;?>/assets/img/girl.jpg"/>
-                </div>
-            </div>
+            <a href="index.html" title="Распродажа">Распродажа</a>
         </li>
         <li>
-            <a>Accessories</a>
-            <div class="dropdown span7">
-                <div class="span2">
-                    <h4>Bags & Purses</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Clutches</a></li>
-                        <li><a href="products-grid.html">Backpacks</a></li>
-                        <li><a href="products-grid.html">Perses</a></li>
-                        <li><a href="products-grid.html">Satches</a></li>
-                        <li><a href="products-grid.html">Shoulder Bags</a></li>
-                        <li><a href="products-grid.html">Desinger Bags</a></li>
-                    </ul>
-                </div>
-                <div class="span2">
-                    <h4>Watches</h4>
-                    <ul>
-                        <li><a href="products-grid.html">Digital Watches</a></li>
-                        <li><a href="products-grid.html">Sports Watches</a></li>
-                        <li><a href="products-grid.html">Chronograph Watches</a></li>
-                        <li><a href="products-grid.html">Designer Watches</a></li>
-                        <li><a href="products-grid.html">Casio Watches</a></li>
-                    </ul>
-                </div>
-                <div class="menu-portrait fr">
-                    <img src="<?php echo $this->theme->baseUrl;?>/assets/img/watch.jpg"/>
-                </div>
-            </div>
+            <a href="index.html" title="Условия заказа">Условия заказа</a>
         </li>
         <li>
-            <a>Pages</a>
-            <div class="dropdown span3">
-                <div class="span2">
-                    <ul>
-                        <li><a href="products-grid.html">Products Grid-view</a></li>
-                        <li><a href="products-list.html">Products List-view</a></li>
-                        <li><a href="product-details.html">Product Details</a></li>
-                        <li><a href="login.html">Account Login</a></li>
-                        <li><a href="registration.html">Account Registration</a></li>
-                        <li><a href="checkout.html">Chechout</a></li>
-                        <li><a href="shortcodes.html">Shortcodes</a></li>
-                        <li><a href="advanced-search.html">Advanced Search</a></li>
-                        <li><a href="contact-us.html">Contact Us</a></li>
-                    </ul>
-                </div>
-            </div>
+            <a href="index.html" title="Акция">Акция</a>
         </li>
-        <li><a href="contact-us.html">Contact Us</a></li>
+        <li><a href="contact-us.html">Контакты</a></li>
     </ul>
     <!--/LISTS-->
 </div>
