0d9284705efc4c25feb3273ca3c2690143167c26
mifort1 <mifort@ukr.net>
2016-09-20 15:50:26 +0300

    headerBar ru

--- frontend/web/themes/rightChoice/views/widgets/HeaderBar/headerBar.php
+++ frontend/web/themes/rightChoice/views/widgets/HeaderBar/headerBar.php
@@ -3,31 +3,19 @@
 <div class="header-bar clearfix">
     <!--HEADER  OPTHION BAR-->
     <div class="container">
-        <!--LANGUAGE AND CURRENCY GROUP-->
-        <div class="header-quick-options fl">
-            <a href="index.html" class="active">en</a>
-            <a href="index.html">fr</a>
-            <a href="index.html">gm</a>
-            <p>&nbsp;
-
-            </p>
-            <a href="index.html" class="active">$</a>
-            <a href="index.html">&euro;</a>
-        </div>
-        <!-- /LANGUAGE AND CURRENCY GROUP-->
         <!--QUICK CONTACT-->
         <div class="header-quick-contact fl">
             <p>
-                <i class="icon-phone icon-2x"></i> Call us: +444(000) 123 45 67
+                <i class="icon-phone icon-2x"></i> Звонок: +444(000) 123 45 67
             </p>
         </div>
         <!--/QUICK CONTACT-->
         <!-- USER SHORTCUTS-->
         <div class="header-user-shortcuts fr">
             <ul>
-                <li><i class="icon-lock"></i> Welcome, visitor.</li>
-                <li><i class="icon-user"></i><a href="index.html">My Account</a></li>
-                <li><i class="icon-heart"></i><a href="index.html">My Wishlist</a></li>
+                <li><i class="icon-lock"></i> Добро пожаловать.</li>
+                <li><i class="icon-user"></i><a href="index.html">Мой аккаунт</a></li>
+                <li><i class="icon-heart"></i><a href="index.html">Мои желания</a></li>
             </ul>
         </div>
         <!-- /USER SHORTCUTS-->
